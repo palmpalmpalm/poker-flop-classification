921a720bb5ce1262e33f7c6842475bf45d2a96f1
Supawich Sitdhipol <Supawichs@gmail.com>
2021-03-24 15:22:34 +0700

    Add set_pair set_flush to Flop

--- flop.py
+++ flop.py
@@ -11,8 +11,8 @@ MED = ['6', '7', '8', '9']
 LOW = ['2', '3', '4', '5']
 group = [[A, 'A'], [HIGH, "H"], [MED, "M"], [LOW, "L"]]
 
-FH = ["TRIPS", "PAIRED", "UNPAIRED"]  # Full Houses Type
-FL = ["RAINBOW", "TWO-TONE", "MONOTONE"]  # Flushes Type
+paired = ["TRIPS", "PAIRED", "UNPAIRED"]  # Paired types
+flush = ["RAINBOW", "TWO-TONE", "MONOTONE"]  # Flushes Type
 STR = ["OESD", "GUTSHOT", "STRAIGHT", "NONE"]  # Straights Type
 
 
@@ -21,10 +21,18 @@ class Flop:
     flop = []
     flop_card = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     flop_suit = [0, 0, 0, 0]
+    group = None
+    paired = None
+    flush = None
+    span = None
 
     # init flop with input flop
     def __init__(self, text):
         self.set_flop(text)
+        self.card_and_suit_count()
+        self.set_group()
+        self.set_fullhouse()
+        self.set_flush()
 
     # set flop
     @classmethod
@@ -34,7 +42,6 @@ class Flop:
         else:
             temp = [text[0:2], text[2:4], text[4:6]]
         cls.flop = temp.copy()
-        cls.card_and_suit_count()
 
     # count card and suit
     @classmethod
@@ -44,14 +51,46 @@ class Flop:
             temp_suit = card[1]
             cls.flop_card[deck.index(temp_card)] += 1
             cls.flop_suit[suit.index(temp_suit)] += 1
-        print(cls.flop_card, cls.flop_suit)
 
-    # return flop
+
+    # set cls.group ex. AHM AMM MML
+    @classmethod
+    def set_group(cls):
+        cards = cls.flop
+        group_cards = ""
+        for card in cards:
+            for group_type in group:
+                if card[0] in group_type[0]:
+                    group_cards += group_type[1]
+                    continue
+        cls.group = group_cards
+
+    # set cls.paired ex. PAIRED UNPAIRED TRIPS
+    @classmethod
+    def set_fullhouse(cls):
+        if 3 in cls.flop_card: # TRIPS
+            cls.paired = paired[0]
+        elif 2 in cls.flop_card: #PAIRED
+            cls.paired = paired[1]
+        else: #UNPAIRED
+            cls.paired = paired[2]
+
+    # set cls.flush ex. MONOTONE TWO-TONE RAINBOW
+    @classmethod
+    def set_flush(cls):
+        if 3 in cls.flop_suit: # MONOTONE
+            cls.flush = flush[2]
+        elif 2 in cls.flop_suit: # TWO-TONE
+            cls.flush = flush[1]
+        else: # RAINBOW
+            cls.flush = flush[0]
+
+    # return flop card only (no suit)
     @classmethod
     def get_flop_card(cls):
-        return cls.flop
+        return cls.flop[0][0] + cls.flop[1][0] + cls.flop[2][0]
 
-    # print flop
+    # print Flop ex. AcTc5d
     def __str__(self):
         return self.flop[0] + self.flop[1] + self.flop[2]
 
@@ -59,3 +98,7 @@ class Flop:
 if __name__ == '__main__':
     t = Flop('AcTc5d')
     print(t)
+    print(t.group)
+    print(t.paired)
+    print(t.flush)
+
